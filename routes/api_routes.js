e8bc612bc3c6b46a14689dc73aac58aae0b49c4c
Shirley-April <shirleyapril23@gmail.com>
2022-06-23 21:58:16 +0300

    md5 hash

--- routes/api_routes.js
+++ routes/api_routes.js
@@ -1,16 +1,20 @@
 const express = require("express")
 const router = express.Router()
+const CryptoJS = require("crypto-js")
 
+// get ip address of client 
 router.get('/ip/jsontest.com', (req, res) => {
     return res.json({
         "ip": req.ip
     })
 })
 
+// request headers received from client
 router.get("/headers/jsontest.com", (req, res) => {
     return res.json(req.headers)
 })
 
+// Date & Time
 router.get("/date/jsontest.com", (req, res) => {
     const date = new Date()
     const month = date.getMonth() + 1
@@ -23,6 +27,23 @@ router.get("/date/jsontest.com", (req, res) => {
         "date": `${month}-${day}-${year}`,
         "milliseconds_since_epoch": millisecondsSinceEpoch,
         "time": date.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true })
+    })
+})
+
+// customized JSON object defined through a REST-style URL
+
+// router.get("/echo/jsontest.com/*", (req, res) => {
+//     return res.json(req.params)
+// })
+
+router.get("/md5/jsontest.com", (req, res) => {
+
+    console.log(req.query.text);
+    //return res.json(req.query.text)
+    const hash = CryptoJS.MD5(req.query.text).toString()
+    return res.json({
+        "original": req.query.text,
+        "md5": hash
     })
 })
 
--- test.js
+++ test.js
@@ -1,4 +1,11 @@
-var time = new Date();
-console.log(
-  time.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true })
-);
+// var time = new Date();
+// console.log(
+//   time.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true })
+// );
+// const CryptoJS = require("crypto-js")
+
+// var hash = CryptoJS.MD5("Message");
+// console.log(hash);
+
+const md = require("crypto-js")
+console.log(md.MD5("[text").toString());
